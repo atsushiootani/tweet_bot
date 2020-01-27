2f9c5aa5e3910ac61926103b11ff9495061ae098
atsushi otani <atsushi.ootani@gmail.com>
2020-01-27 12:42:41 +0900

    ひとまずツイートのCRUDをするためのDBとページを作成

--- app/infrastructure/db.go
+++ app/infrastructure/db.go
@@ -1,7 +1,17 @@
 package infrastructure
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
+)
 
+type Tweet struct {
+	gorm.Model
+	Text   string // 文面
+	Status string // 投稿したか
+	TweetAt time.Time // 投稿する日時
+}
 
 func dbOpenConnection() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "db/test.sqlite3")
@@ -11,10 +21,55 @@ func dbOpenConnection() *gorm.DB {
 	return db
 }
 
-//DB初期化
 func DbInit() {
 	db := dbOpenConnection()
 	defer db.Close()
 
-	//db.AutoMigrate(&Todo{})
+	db.AutoMigrate(&Tweet{})
+}
+
+func DbInsert(text string, status string, tweetAt time.Time) {
+	db := dbOpenConnection()
+	defer db.Close()
+
+	db.Create(&Tweet{Text: text, Status: status, TweetAt: tweetAt})
+}
+
+func DbUpdate(id int, text string, status string, tweetAt time.Time) {
+	db := dbOpenConnection()
+	defer db.Close()
+
+	var tweet Tweet
+	db.First(&tweet, id)
+	tweet.Text = text
+	tweet.Status = status
+	tweet.TweetAt = tweetAt
+	db.Save(&tweet)
+}
+
+func DbDelete(id int) {
+	db := dbOpenConnection()
+	defer db.Close()
+
+	var tweet Tweet
+	db.First(&tweet, id)
+	db.Delete(&tweet)
+}
+
+func DbGetAll() []Tweet {
+	db := dbOpenConnection()
+	defer db.Close()
+
+	var tweets []Tweet
+	db.Order("created_at desc").Find(&tweets)
+	return tweets
+}
+
+func DbGetOne(id int) Tweet {
+	db := dbOpenConnection()
+	defer db.Close()
+
+	var tweet Tweet
+	db.First(&tweet, id)
+	return tweet
 }
--- main.go
+++ main.go
@@ -3,17 +3,81 @@ package main
 import (
 	"./app/infrastructure"
 	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
-	_ "strconv"
+	"strconv"
+	"time"
 )
 
 func main() {
 	infrastructure.DbInit()
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context){
-		c.String(200, "Hello, world!")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	//Index
+	router.GET("/", func(ctx *gin.Context) {
+		tweets := infrastructure.DbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"tweets": tweets,
+		})
 	})
-	r.Run()
+
+	//Create
+	router.POST("/new", func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		// tweetAt := ctx.PostForm("tweetAt") // TODO
+		infrastructure.DbInsert(text, status, time.Now())
+		ctx.Redirect(302, "/")
+	})
+
+	//Detail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		tweet := infrastructure.DbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"tweet": tweet})
+	})
+
+	//Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		// tweetAt := ctx.PostForm("tweetAt") //TODO
+		infrastructure.DbUpdate(id, text, status, time.Now())
+		ctx.Redirect(302, "/")
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		tweet := infrastructure.DbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"tweet": tweet})
+	})
+
+	//Delete
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		infrastructure.DbDelete(id)
+		ctx.Redirect(302, "/")
+
+	})
+
+	router.Run()
 
 }
